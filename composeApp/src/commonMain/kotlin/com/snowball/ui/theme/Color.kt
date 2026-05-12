5bf3e7edf8389737fdc894fe6db91e378e4585ad
shixumi <ldenzell88@gmail.com>
2026-05-12 18:15:14 +0800

    feat(theme): snow color palette

---
+++ composeApp/src/commonMain/kotlin/com/snowball/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.snowball.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object SnowColors {
+    val Night = Color(0xFF0B0F14)
+    val NightElev = Color(0xFF11161D)
+    val CardElev = Color(0xFF161C25)
+    val Line = Color(0x12FFFFFF)
+    val LineStrong = Color(0x21FFFFFF)
+    val Frost = Color(0xFFF2F5F8)
+    val FrostMute = Color(0xFFA8B2BF)
+    val FrostDim = Color(0xFF5E6874)
+    val FrostDeep = Color(0xFF3C4452)
+    val Ice = Color(0xFF9FCEE3)
+    val IceSoft = Color(0x299FCEE3)
+    val Champagne = Color(0xFFE8C68A)
+    val Ember = Color(0xFFE07856)
+    val Green = Color(0xFF8FD9B2)
+}
